0ec0d70467766e68f56163a0e6bd5b371785effb
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-02 13:00:07 +0300

    Creating and registering blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,15 @@ def create_app(config_name):
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
     # temporary route
     @app.route('/')
     def hello_world():
